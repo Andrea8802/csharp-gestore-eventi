a0255a8fa295a7f80673e8f4d088dd48ec6d50c2
Andrea Bonaventura <andreabonaventura802@gmail.com>
2023-04-17 13:37:26 +0200

    Milestone 3

--- csharp-gestore-eventi/ProgrammaEventi.cs
+++ csharp-gestore-eventi/ProgrammaEventi.cs
@@ -10,12 +10,55 @@ namespace csharp_gestore_eventi
     {
         public string Titolo { get; set; }
 
-        List <Evento> eventi = new List <Evento> ();
+        public List<Evento> eventi;
 
         public ProgrammaEventi(string titolo)
         {
             Titolo = titolo;
-          
+            eventi = new List<Evento>();
+        }
+
+        public void AggiungiEvento(Evento evento)
+        {
+            eventi.Add(evento);
+        }
+
+        public List<Evento> CercaPerData(DateTime dataCercata)
+        {
+            var eventiCercati = eventi.Where(evento => evento.Data == dataCercata).ToList();
+            return eventiCercati;
+        }
+
+        public static string EventiToString(List<Evento> eventi)
+        {
+            StringBuilder listaStringa = new StringBuilder();
+
+            foreach(Evento evento in eventi)
+            {
+                listaStringa.AppendLine($"Titolo: {evento.Titolo}, Data: {evento.Data}, Posti Prenotati: {evento.PostiPrenotati}/{evento.CapienzaMax}");
+            }
+
+            return listaStringa.ToString();
+        }
+
+        public int CounterEventi(List <Evento> eventi)
+        {
+            return eventi.Count;
+        }
+
+        public void SvuotaListaEventi(List <Evento> eventi)
+        {
+            eventi.Clear();
+        }
+
+        public string ShowTitoloProgramma(List <Evento> eventi)
+        {
+            StringBuilder titoloProgramma = new StringBuilder();
+
+            foreach(Evento evento in eventi)
+            {
+                titoloProgramma.AppendLine($"{evento.Data} - {evento.Titolo}\n")
+            }
         }
     }
 }
